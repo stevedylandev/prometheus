9533e08401fad8e9d1fd4720abf1dc15b1183c08
Steve Simkins <stevedsimkins@gmail.com>
2025-07-17 23:40:39 -0400

    chore: Added biome and initial login

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
 import logo from "./assets/logo.png";
 import { Button } from "./components/ui/button";
+import { useAccount, useConnect, useDisconnect } from "wagmi";
 
 function App() {
+	const account = useAccount();
+	const { disconnect } = useDisconnect();
+
+	const { connectors, connect } = useConnect();
+	const connector = connectors.find(
+		(connector) => connector.id === "xyz.ithaca.porto",
+	)!;
+
 	return (
 		<main className="min-h-screen flex flex-col items-center justify-center">
 			<div className="max-w-lg flex flex-col items-center gap-4">
-				<img src={logo} alt="promethus logo" />
-				<h1 className="text-4xl">Prometheus</h1>
-				<h3>Open Source EVM Wallets</h3>
-				<Button className="mt-4" disabled>
-					Coming Soon
-				</Button>
+				{account.address ? (
+					<div className="flex flex-col items-center justify-center gap-4">
+						<div>
+							{account.address.slice(0, 6)}...{account.address.slice(-4)}
+						</div>
+						<Button type="button" onClick={() => disconnect()}>
+							Sign out
+						</Button>
+					</div>
+				) : (
+					<>
+						<img src={logo} alt="promethus logo" />
+						<h1 className="text-4xl">Prometheus</h1>
+						<h3>Open Source EVM Wallets</h3>
+						<Button onClick={() => connect({ connector })}>Sign in</Button>
+					</>
+				)}
 			</div>
 		</main>
 	);
